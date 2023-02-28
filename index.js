4555230dbc1a926d829f8e3cf8edb6b0c52b3bd3
Dnyaneshwar <dnyaneshwarpk20@gmail.com>
2023-02-28 19:06:03 +0530

    Commit 2

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 for (i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", handleclick);
 }
@@ -16,31 +15,31 @@ document.addEventListener("keypress", function (event) {
 function makesound(key) {
     switch (key) {
         case "w":
-            var audio = new Audio('tom-1.mp3');
+            var audio = new Audio('Sounds/tom-1.mp3');
             audio.play();
             break;
         case "a":
-            var audio = new Audio('tom-2.mp3');
+            var audio = new Audio('Sounds/tom-2.mp3');
             audio.play();
             break;
         case "s":
-            var audio = new Audio('tom-3.mp3');
+            var audio = new Audio('Sounds/tom-3.mp3');
             audio.play();
             break;
         case "d":
-            var audio = new Audio('tom-4.mp3');
+            var audio = new Audio('Sounds/tom-4.mp3');
             audio.play();
             break;
         case "j":
-            var audio = new Audio('snare.mp3');
+            var audio = new Audio('Sounds/snare.mp3');
             audio.play();
             break;
         case "k":
-            var audio = new Audio('crash.mp3');
+            var audio = new Audio('Sounds/crash.mp3');
             audio.play();
             break;
         case "l":
-            var audio = new Audio('kick-bass.mp3');
+            var audio = new Audio('Sounds/kick-bass.mp3');
             audio.play();
             break;
 
